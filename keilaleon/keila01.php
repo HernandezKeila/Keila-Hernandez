dd228065bf08d19e544a1fad07ef32e63acd7ac6
Keila Zeret Hernández León <a23328061310021@cetis131.edu.mx>
2025-02-13 13:22:08 -0600

    Semana 2

---
+++ keilaleon/keila01.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap-theme.min.css">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="style.css">
+    <title>Keila Zeret Hernández León</title>
+</head>
+<body>
+    <link href="https://fonts.cdnfonts.com/css/apes-on-parade" rel="stylesheet">
+    <link href="https://fonts.cdnfonts.com/css/ningst-sparkle" rel="stylesheet">
+    <nav class="navbar navbar-light" style="background-color: #84c047;">
+    
+    <div class="container">
+        <a class="navbar-brand" href="#" style="color: #ecf39e">Inicio</a>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="nav navbar-nav">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarNavDropdownMenuLink" 
+                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="color:#ecf39e">
+                    Unidad 1
+                    </a>
+                    <!--lo que sigue son los menus que se van  a desplegar hacia abajo,cado uno tendra el numero -->
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink" >
+                        <a class="dropdown-item" href="/keilaleon/keila01.php" >Practica 1</a><br>
+                        <a class="dropdown-item" href="/keilaleon/keila02.php" >Practica 2</a><br>
+                        <a class="dropdown-item" href="./keilaleon/Keila03.html" >Practica 3</a><br>
+                    </div>
+                </li>
+
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarNavDropdownMenuLink" 
+                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="color:#ecf39e">
+                    Unidad 2
+                    </a>
+                    <!--lo que sigue son los menus que se van  a desplegar hacia abajo,cado uno tendra el numero -->
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <a class="dropdown-item" href="./Keila04.html" >Practica 4</a><br>
+                        <a class="dropdown-item" href="./Keila05.html" >Practica 5</a><br>
+                        <a class="dropdown-item" href="./Keila06.html" >Practica 6</a><br>
+                    </div>
+                </li>
+
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarNavDropdownMenuLink" 
+                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="color:#ecf39e">
+                    Unidad 3
+                    </a>
+                    <!--lo que sigue son los menus que se van  a desplegar hacia abajo,cado uno tendra el numero -->
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <a class="dropdown-item" href="./Keila07.html" >Practica 7</a><br>
+                        <a class="dropdown-item" href="./Keila08.html" >Practica 8</a><br>
+                        <a class="dropdown-item" href="./Keila09.html" >Practica 9</a><br>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+    <div class="jumbotron text-center" style="background-color: white;">
+        <h1 class="display-4" style="font-family:'Apes On Parade', sans-serif; color: #0d8eb2;">Studio Ghibli</h1>
+        
+    
+    </div>
+
+    <div class="row">
+        <div class="col-sm-2">
+            <div class="card text-white bg-success mb-3" style="max-width: 18rem;">
+                <div class="card-header">Header</div>
+                <div class="card-body">
+                    <h5 class="card-title">Success card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
+        </div>  
+
+        <div class="col-sm-2">
+            <div class="card text-white bg-success mb-3" style="max-width: 18rem;">
+                <div class="card-header">Header</div>
+                <div class="card-body">
+                    <h5 class="card-title">Success card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
+        </div>  
+
+        <div class="col-sm-2">
+            <div class="card text-white bg-success mb-3" style="max-width: 18rem;">
+                <div class="card-header">Header</div>
+                <div class="card-body">
+                    <h5 class="card-title">Success card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
+        </div>
+        
+        <div class="col-sm-2">
+            <div class="card text-white bg-success mb-3" style="max-width: 18rem;">
+                <div class="card-header">Header</div>
+                <div class="card-body">
+                    <h5 class="card-title">Success card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
+        </div>  
+
+        <div class="col-sm-2">
+            <div class="card text-white bg-success mb-3" style="max-width: 18rem;">
+                <div class="card-header">Header</div>
+                <div class="card-body">
+                    <h5 class="card-title">Success card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
+        </div>  
+
+        <div class="col-sm-2">
+            <div class="card text-white bg-success mb-3" style="max-width: 18rem;">
+                <div class="card-header">Header</div>
+                <div class="card-body">
+                    <h5 class="card-title">Success card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
+        </div>  
+    </div>  
+</div>
+
+</body>
+</html>
